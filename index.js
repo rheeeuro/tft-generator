ad8cec6b523b8e1e4015b1fb88e416524d861c0e
Rhee Euro <eurohand@naver.com>
2024-11-26 13:33:42 +0900

    make team button

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { SYNERGY_INFO, UNIT, UNIT_INFO } from "./data.js";
+import { SYNERGY_INFO, UNIT_INFO } from "./data13.js";
 import { Queue } from "./queue.js";
 
 const MAX_LEVEL = 5;
@@ -8,28 +8,65 @@ let initialSynergyStaus = [];
 let initialUnitStatus = [];
 let result = [];
 
-const levelFilter = (level, unitStatus) => {
+// 해당 레벨에서 확률이 희박한 코스트 유닛은 제외
+const levelFilter = (unitStatus) => {
   return (unit) => {
     if (unitStatus[unit.id]) return false;
 
-    if (level < 5) {
+    if (MAX_LEVEL < 8) {
       return unit.cost < 4;
-    } else if (level < 7) {
+    } else if (MAX_LEVEL < 9) {
       return unit.cost < 5;
     }
     return true;
   };
 };
 
-const initialize = (level) => {
+const initializeArray = () => {
+  initialSynergyStaus = [];
+  initialUnitStatus = [];
+  result = [];
+
   for (let i = 0; i <= SYNERGY_INFO.length; i++) {
     initialSynergyStaus.push(0);
   }
   for (let i = 0; i <= UNIT_INFO.length; i++) {
     initialUnitStatus.push(false);
   }
+};
 
-  UNIT_INFO.filter(levelFilter(level, initialUnitStatus)).forEach((unit) => {
+// 큐 초기화 후 유닛 1개 insert
+const initialize = (selectedUnitId) => {
+  initializeArray();
+  if (selectedUnitId) {
+    const selectedUnit = UNIT_INFO[selectedUnitId];
+    const synergyStatus = [...initialSynergyStaus];
+    const unitStatus = [...initialUnitStatus];
+    unitStatus[selectedUnitId] = true;
+    selectedUnit.synergy.forEach((synergy) => {
+      synergyStatus[synergy] += 1;
+    });
+
+    UNIT_INFO.filter(levelFilter(unitStatus)).forEach((unit) => {
+      const newSynergyStatus = [...synergyStatus];
+      const newUnitStatus = [...unitStatus];
+      newUnitStatus[unit.id] = true;
+      unit.synergy.forEach((synergy) => {
+        newSynergyStatus[synergy] += 1;
+      });
+      q.enqueue({
+        level: 2,
+        totalCost: selectedUnit.cost + unit.cost,
+        synergyStatus: newSynergyStatus,
+        unitStatus: newUnitStatus,
+        lastUnitId: unit.id,
+      });
+    });
+
+    return;
+  }
+
+  UNIT_INFO.filter(levelFilter(initialUnitStatus)).forEach((unit) => {
     const synergyStatus = [...initialSynergyStaus];
     const unitStatus = [...initialUnitStatus];
     unitStatus[unit.id] = true;
@@ -46,6 +83,7 @@ const initialize = (level) => {
   });
 };
 
+// 점수 계산: 유닛 총 코스트 + 시너지 유닛 수 - (3* 시너지 미적용 유닛 수)
 const addScore = (status) => {
   let synergyScore = 0;
   SYNERGY_INFO.filter(
@@ -64,61 +102,72 @@ const addScore = (status) => {
   return { ...status, score: status.totalCost + synergyScore };
 };
 
-for (let level = 5; level <= MAX_LEVEL; level += 1) {
-  initialize(level);
-  while (!q.isEmpty()) {
-    const current = q.dequeue();
-    if (current.level < level) {
-      UNIT_INFO.filter((unit) => current.lastUnitId < unit.id)
-        .filter(levelFilter(current.level, current.unitStatus))
-        .forEach((unit) => {
-          const synergyStatus = [...current.synergyStatus];
-          const unitStatus = [...current.unitStatus];
-          unitStatus[unit.id] = true;
-          unit.synergy.forEach((synergy) => {
-            synergyStatus[synergy] += 1;
+const makeTeam = (unitId) => {
+  return () => {
+    initialize(unitId);
+    while (!q.isEmpty()) {
+      const current = q.dequeue();
+      if (current.level < MAX_LEVEL) {
+        UNIT_INFO.filter((unit) => current.lastUnitId < unit.id)
+          .filter(levelFilter(current.unitStatus))
+          .forEach((unit) => {
+            const synergyStatus = [...current.synergyStatus];
+            const unitStatus = [...current.unitStatus];
+            unitStatus[unit.id] = true;
+            unit.synergy.forEach((synergy) => {
+              synergyStatus[synergy] += 1;
+            });
+            q.enqueue(
+              addScore({
+                level: current.level + 1,
+                totalCost: current.totalCost + unit.cost,
+                synergyStatus,
+                unitStatus,
+                lastUnitId: unit.id,
+              })
+            );
           });
-          q.enqueue(
-            addScore({
-              level: current.level + 1,
-              totalCost: current.totalCost + unit.cost,
-              synergyStatus,
-              unitStatus,
-              lastUnitId: unit.id,
-            })
-          );
-        });
-    } else {
-      result.push(current);
+      } else {
+        result.push(current);
+      }
     }
-  }
-}
+    console.log(result.sort((a, b) => b.score - a.score).map(printResult));
+  };
+};
 
-console.log(
-  result
-    .sort((a, b) => b.score - a.score)
-    .map((status) => {
-      const synergyTextList = SYNERGY_INFO.filter(
-        (synergy) => status.synergyStatus[synergy.id] >= synergy.condition[0]
-      ).map((synergy) => {
-        let count = 0;
-        for (const cond of synergy.condition) {
-          if (cond <= status.synergyStatus[synergy.id]) {
-            count = cond;
-          } else {
-            break;
-          }
-        }
-        return `${count}${synergy.displayName}`;
-      });
-      const unitTextList = UNIT_INFO.filter(
-        (unit) => status.unitStatus[unit.id]
-      ).map((unit) => unit.displayName);
+// 결과 출력
+const printResult = (status) => {
+  const synergyTextList = SYNERGY_INFO.filter(
+    (synergy) => status.synergyStatus[synergy.id] >= synergy.condition[0]
+  ).map((synergy) => {
+    let count = 0;
+    for (const cond of synergy.condition) {
+      if (cond <= status.synergyStatus[synergy.id]) {
+        count = cond;
+      } else {
+        break;
+      }
+    }
+    return `${count}${synergy.displayName}`;
+  });
+  const unitTextList = UNIT_INFO.filter(
+    (unit) => status.unitStatus[unit.id]
+  ).map((unit) => unit.displayName);
 
-      return [
-        status.score + "점",
-        [...unitTextList].join(","),
-        synergyTextList.join(","),
-      ];
-    })
-);
+  return [
+    status.score + "점",
+    [...unitTextList].join(","),
+    synergyTextList.join(","),
+  ];
+};
+
+const init = () => {
+  UNIT_INFO.forEach((unit) => {
+    const button = document.createElement("button");
+    button.innerText = unit.name;
+    button.addEventListener("click", makeTeam(unit.id));
+    document.body.appendChild(button);
+  });
+};
+
+init();
